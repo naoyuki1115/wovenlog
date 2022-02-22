6a3f44c4a64a05aa118a853103fdd1ede4e306b2
naoyuki-saka <naoyuki.saka@woven-planet.global>
2022-02-22 11:45:02 +0900

    WV-19: first commit

--- lib/screens/spot_list_screen.dart
+++ lib/screens/spot_list_screen.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class SpotListScreen extends StatelessWidget {
+  const SpotListScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      
+    );
+  }
+}
