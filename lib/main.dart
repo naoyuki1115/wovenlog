c99f92dbcbaa417489df284ecc29745206cbd0bb
yoshiaki-toyama <yoshiaki.toyama@woven-planet.global>
2022-02-28 12:27:39 +0900

    WV-10-03: add go_router in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 
-import '../screens/login_screen.dart';
-import '../screens/top_screen.dart';
-import '../dummy_data/selected_category_list.dart';
+import './screens/login_screen.dart';
+import './screens/top_screen.dart';
+import './screens/top_edit_screen.dart';
+import './screens/spot_list_screen.dart';
+import './screens/spot_detail_screen.dart';
+import './screens/spot_post_screen.dart';
+import './dummy_data/selected_category_list.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final _router = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/top_screen',
+        builder: (context, state) => const TopScreen(),
+      ),
+      GoRoute(
+        path: '/top_edit_screen',
+        builder: (context, state) => const TopEditScreen(),
+      ),
+      GoRoute(
+        path: '/spot_list_screen',
+        builder: (context, state) => const SpotListScreen(),
+        // path: '/spot_list_screen/:categoryId',
+        // builder: (context, state) => SpotListScreen(categoryId: state.params['categoryId']!),
+      ),
+      GoRoute(
+        path: '/spot_detail_screen/:spotId',
+        builder: (context, state) => SpotDetailScreen(spotId: state.params['spotId']!),
+      ),
+      GoRoute(
+        path: '/spot_post_screen',
+        builder: (context, state) => const SpotPostScreen(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      child: const MaterialApp(
-        title: 'WovenLog',
-        home: TopScreen(),
+      child: MaterialApp.router(
+        routeInformationParser: _router.routeInformationParser,
+        routerDelegate: _router.routerDelegate,
       ),
       providers: [
         ChangeNotifierProvider(
@@ -27,3 +64,19 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MultiProvider(
+  //     child: const MaterialApp(
+  //       title: 'WovenLog',
+  //       home: TopScreen(),
+  //     ),
+  //     providers: [
+  //       ChangeNotifierProvider(
+  //         create: (context) => SelectedCategoryList(),
+  //       )
+  //     ],
+  //   );
+  // }
